bbe09fd9cbf035b73f25a9ec581e79a9f91c2858
HungHa <hungtmt1102@gmail.com>
2025-04-26 21:22:01 +0700

    Fix cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -48,10 +48,17 @@ return Application::configure(basePath: dirname(__DIR__))
         //     ], Response::HTTP_NOT_FOUND);
         // });
 
-        // $exceptions->render(function (\Throwable $e, $request) {
-        //     return response()->json([
-        //         'message' => 'Đã xảy ra lỗi, vui lòng thử lại sau.',
-        //         'error' => config('app.debug') ? $e->getMessage() : null,
-        //     ], Response::HTTP_INTERNAL_SERVER_ERROR);
-        // });
+        $exceptions->render(function (\Throwable $e, $request) {
+            \Log::error('Lỗi xảy ra: ' . $e->getMessage(), [
+                'exception' => $e,
+                'stack_trace' => $e->getTraceAsString(),
+                'url' => $request->url(),
+                'input' => $request->all(),
+            ]);
+
+            return response()->json([
+                'message' => 'Đã xảy ra lỗi, vui lòng thử lại sau.',
+                'error' => config('app.debug') ? $e->getMessage() : null,
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        });
     })->create();
