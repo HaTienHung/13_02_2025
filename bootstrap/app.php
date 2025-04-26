2ec2eedcc6eeba415ab9e3957de4fac4a929f724
HungHa <hungtmt1102@gmail.com>
2025-04-26 20:48:01 +0700

    Test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -48,10 +48,10 @@ return Application::configure(basePath: dirname(__DIR__))
         //     ], Response::HTTP_NOT_FOUND);
         // });
 
-        $exceptions->render(function (\Throwable $e, $request) {
-            return response()->json([
-                'message' => 'Đã xảy ra lỗi, vui lòng thử lại sau.',
-                'error' => config('app.debug') ? $e->getMessage() : null,
-            ], Response::HTTP_INTERNAL_SERVER_ERROR);
-        });
+        // $exceptions->render(function (\Throwable $e, $request) {
+        //     return response()->json([
+        //         'message' => 'Đã xảy ra lỗi, vui lòng thử lại sau.',
+        //         'error' => config('app.debug') ? $e->getMessage() : null,
+        //     ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        // });
     })->create();
